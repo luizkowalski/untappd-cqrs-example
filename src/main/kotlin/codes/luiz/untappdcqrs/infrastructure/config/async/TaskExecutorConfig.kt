3c326734132ccc994c48e0abb7f5cbbf2ad1e4ba
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2018-06-12 15:36:37 +0200

    configured task executor properly

--- src/main/kotlin/codes/luiz/untappdcqrs/infrastructure/config/async/TaskExecutorConfig.kt
+++ src/main/kotlin/codes/luiz/untappdcqrs/infrastructure/config/async/TaskExecutorConfig.kt
@@ -1,22 +1,23 @@
 package codes.luiz.untappdcqrs.infrastructure.config.async
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.task.AsyncTaskExecutor
+import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
 
 
 @Configuration
-class TaskExecutorConfig {
+class TaskExecutorConfig : AsyncConfigurer {
 
-  @Bean
-  fun getAsync(): AsyncTaskExecutor {
+  override fun getAsyncExecutor(): Executor {
     val executor = ThreadPoolTaskExecutor()
     val cores = Runtime.getRuntime().availableProcessors()
     executor.corePoolSize = cores
     executor.maxPoolSize = cores * 2
     executor.setQueueCapacity(cores * 200)
-    executor.setThreadNamePrefix("untappd-cqrs-threadpool-")
+    executor.setThreadNamePrefix("untappd-cqrs-tp-")
+    executor.initialize()
+
     return executor
   }
 }
